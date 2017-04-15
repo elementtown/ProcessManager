9ecd2808eceda953aea5fcacc2ef306e98e22ddb
Matthew Oltendorf <matthew@oltendorf.net>
2017-04-14 20:46:27 -0700

    Added comments for link to Floobits from Facebook

--- src/main/kotlin/xyz/sequence/processmanager/ProcessManager.kt
+++ src/main/kotlin/xyz/sequence/processmanager/ProcessManager.kt
@@ -2,7 +2,18 @@ package xyz.sequence.processmanager
 
 /**
  * Created by moltendorf on 2017-04-08.
+ *
+ * Feel free to add comments if you're curious.
  */
+
+/* This is a comment. Notice how it starts and ends.
+   They can also be multi-line in this style. */
+
+// This is also a comment, it goes until the end of the line.
+// For multiple lines in this style just add multiple // to each line.
+
+// While everyone can edit this repository, since it will be production code on my server, everything is safely checked and committed via Git.
+// I'll keep the comments though (since they won't actually break any code if you're responsible).
 
 fun main(args: Array <String>) {
   println("Hello world!")
