84cd2a260c36bdf9f70a68ac5e0c9ebf3fb65f9e
Matthew Oltendorf <matthew@oltendorf.net>
2017-04-14 20:30:58 -0700

    Updated code style settings

--- src/main/kotlin/xyz/sequence/processmanager/ProcessManager.kt
+++ src/main/kotlin/xyz/sequence/processmanager/ProcessManager.kt
@@ -4,6 +4,6 @@ package xyz.sequence.processmanager
  * Created by moltendorf on 2017-04-08.
  */
 
-fun main(args: Array < String > ) {
-    println("Hello world!")
+fun main(args: Array <String>) {
+  println("Hello world!")
 }
